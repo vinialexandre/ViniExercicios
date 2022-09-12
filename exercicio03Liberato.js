0aa2e2fcf96b7f089218a73bb9e075f8ced47f43
Adriana-Drika <nmdrika@gmail.com>
2022-09-12 20:10:55 -0300

    atualização

--- exercicio03Liberato.js
+++ exercicio03Liberato.js
@@ -188,9 +188,6 @@ console.log(alistamentoMilitar(ano_Atual , ano_nascimento));
 
 
 
-
-
-
 /*
 8. Faça um algoritmo que pergunte a distância que um passageiro deseja percorrer
 em Km. Calcule o preço da passagem, cobrando R$0.50 por Km para viagens até
